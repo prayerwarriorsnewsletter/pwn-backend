55b6eb561476c2f4ff21be10cf7c2179fd7512b3
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-03-19 21:03:33 -0700

    added firebase admin and firebase config file

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const server = express();
 
 server.use(cors())
 server.use(express.json())
+server.options('*', cors())
 
+
+authorized = true
+
+function checkAuth(req, res, next){
+    if (authorized){
+        next()
+    }else{
+        res.status(403).send("Unauthorized")
+    }
+}
+
+server.use('/', checkAuth)
 server.get('/', (req, res) => {
     res.status(200).json({message: 'testing get endpoint'})
 })
