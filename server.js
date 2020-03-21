7f1328c6a3dc986824977117defc5b54f0ef9c27
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2020-03-20 19:54:52 -0700

    firebase protected endpoint set up on backend

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const admin = require('firebase-admin')
+const serviceAccount = require("./config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://prayerwarriorsnewsletter.firebaseio.com"
+});
+
 const server = express();
 
 server.use(cors())
@@ -8,11 +15,16 @@ server.use(express.json())
 server.options('*', cors())
 
 
-authorized = true
+// authorized = true
 
 function checkAuth(req, res, next){
-    if (authorized){
-        next()
+    if (req.headers.authtoken){
+        admin.auth().verifyIdToken(req.headers.authtoken)
+        .then(() => {
+            next()
+        }).catch(() => {
+            res.status(403).send("Unauthorized")
+        })
     }else{
         res.status(403).send("Unauthorized")
     }
