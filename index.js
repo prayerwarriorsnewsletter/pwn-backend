5fc3d6bb3ae96be1f936e1591b4746857d18031b
brettmadrid <brettmadrid9@gmail.com>
2020-04-13 21:15:18 -0700

    added process.env.PORT to index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const server = require('./server.js')
+require("dotenv").config();
+const server = require("./server.js");
 
-const port = 5000
-server.listen(port, () => console.log(`Api listenimg on port ${port}`))
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Api listenimg on port ${port}`));
