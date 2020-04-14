0be67f3a2963611f48d85951c3730bcef09fe630
brettmadrid <brettmadrid9@gmail.com>
2020-04-13 21:12:30 -0700

    created tables in migration file

--- data/migrations/20200318202636_first-migration-001.js
+++ data/migrations/20200318202636_first-migration-001.js
@@ -1,8 +1,27 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.text("email", 128).notNullable();
+      tbl.text("password", 128).notNullable();
+      tbl.timestamps(true, true);
+    })
+    .createTable("requests", (tbl) => {
+      tbl.increments();
+      tbl.text("text").notNullable();
+      tbl.timestamps(true, true);
 
-exports.up = function(knex) {
-  
+      tbl
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("requests");
 };
