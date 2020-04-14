cd7f7d640214823eafea893f38b05b6719ef6f7a
brettmadrid <brettmadrid9@gmail.com>
2020-04-13 21:20:25 -0700

    added requestsRouter path to server.js

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const admin = require('firebase-admin')
+const express = require("express");
+const cors = require("cors");
+const admin = require("firebase-admin");
 const serviceAccount = require("./config/fbServiceAccountKey.json");
 
+const requestsRouter = require("./requests/requestRouter");
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://prayerwarriorsnewsletter.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://prayerwarriorsnewsletter.firebaseio.com",
 });
 
 const server = express();
 
-server.use(cors())
-server.use(express.json())
-server.options('*', cors())
+server.use(cors());
+server.use(express.json());
+server.options("*", cors());
 
-
-function checkAuth(req, res, next){
-    if (req.headers.authtoken){
-        admin.auth().verifyIdToken(req.headers.authtoken)
-        .then(() => {
-            next()
-        }).catch(() => {
-            res.status(403).send("Unauthorized")
-        })
-    }else{
-        res.status(403).send("Unauthorized")
-    }
+function checkAuth(req, res, next) {
+  if (req.headers.authtoken) {
+    admin
+      .auth()
+      .verifyIdToken(req.headers.authtoken)
+      .then(() => {
+        next();
+      })
+      .catch(() => {
+        res.status(403).send("Unauthorized");
+      });
+  } else {
+    res.status(403).send("Unauthorized");
+  }
 }
 
-server.use('/', checkAuth)
-server.get('/', (req, res) => {
-    res.status(200).json({message: 'testing get endpoint'})
-})
+server.use("/", checkAuth);
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "testing get endpoint" });
+});
 
-module.exports = server
+server.use("/api/requests", requestsRouter);
+
+module.exports = server;
